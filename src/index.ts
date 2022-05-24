2d83920545103b6e936e2eb791aaf3d6a161a5ee
Dessader <dessader.live@gmail.com>
2022-05-24 16:41:28 +0300

    Uploaded icons

--- src/index.ts
+++ src/index.ts
@@ -9,4 +9,14 @@ import "./views/styles/app";
 * Import icons for generate SVG sprite
 */
 
-// ...
+import "./assets/images/icons/check.svg";
+import "./assets/images/icons/chevron-up.svg";
+import "./assets/images/icons/chevron-left.svg";
+import "./assets/images/icons/chevron-down.svg";
+import "./assets/images/icons/chevron-right.svg";
+import "./assets/images/icons/dock-search.svg";
+import "./assets/images/icons/map-point.svg";
+import "./assets/images/icons/search.svg";
+import "./assets/images/icons/facebook.svg";
+import "./assets/images/icons/twitter.svg";
+import "./assets/images/icons/linkedin.svg";
