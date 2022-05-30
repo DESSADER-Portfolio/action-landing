76d5f0841acdcb0384a4f4428c28f2867e225026
Dessader <dessader.live@gmail.com>
2022-05-30 13:26:50 +0300

    Implemented selectField component

--- src/views/components/components.ts
+++ src/views/components/components.ts
@@ -5,3 +5,10 @@
 export { default as Navbar } from "./navbar/navbar";
 export type { INavbar } from "./navbar/navbar";
 export type { NavbarOptions } from "./navbar/navbar";
+
+/*
+* UI & Controls
+*/
+
+export { default as SelectField } from "./selectField/selectField";
+export type { ISelectField } from "./selectField/selectField";
---
+++ src/views/components/selectField/selectField.ts
@@ -0,0 +1,91 @@
+import { gsap } from "gsap";
+
+export interface ISelectField {
+  element: string;
+  selected?: string;
+}
+
+class SelectField implements ISelectField {
+  public element: string;
+  public selected: string;
+
+  private select: HTMLDivElement;
+  private selectValue: HTMLLIElement;
+  private selectIsOpen: boolean;
+  private rootClassName: string;
+
+  private control: HTMLDivElement;
+  private list: HTMLUListElement;
+  private options: HTMLLIElement[];
+
+  constructor(element: string, selected?: string) {
+    this.select = document.querySelector(element);
+    this.selected = selected;
+    this.selectIsOpen = false;
+    this.rootClassName = this.select.classList[0];
+
+    this.control = this.select.querySelector(`.${this.rootClassName}__control`);
+    this.list = this.select.querySelector(`.${this.rootClassName}__list`);
+    this.options = Array.from(this.list.querySelectorAll(`.${this.rootClassName}__list-item`));
+
+    this.init();
+  }
+  
+  private init() {
+    if(this.selected) {
+      const option: HTMLLIElement = this.list.querySelector(`[data-value='${this.selected}']`);
+      if(option) {
+        this.setValue(option);
+      }
+    }
+
+    this.select.addEventListener("click", () => {
+      this.selectIsOpen ? this.close() : this.open();
+    });
+
+    this.options.forEach((option) => {
+      option.addEventListener("click", () => this.setValue(option));
+    });
+  }
+
+  public setValue(option: HTMLLIElement) {
+    if(this.selectValue) {
+      this.selectValue.classList.remove("selected");
+    }
+
+    this.selectValue = option;
+    this.selectValue.classList.add("selected");
+    this.control.innerHTML = option.innerText;
+    this.select.setAttribute("data-selected", this.selectValue.getAttribute("data-value"));
+  }
+
+  public open() {
+    this.selectIsOpen = true;
+    gsap.to(this.list, {
+      display: "block",
+      opacity: 1,
+      transform: "translateY(0)",
+      duration: 0.2,
+      ease: "back"
+    });
+  }
+
+  public close() {
+    this.selectIsOpen = false;
+    gsap.to(this.list, {
+      display: "none",
+      opacity: 0,
+      transform: "translateY(-30px)",
+      duration: 0.2,
+      ease: "back"
+    });
+  }
+
+  public getValue() {
+    if(this.selectValue) {
+      return this.selectValue.getAttribute("data-value");
+    }
+  }
+}
+
+export default SelectField;
