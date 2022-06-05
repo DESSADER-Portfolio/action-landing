b225e995d9f0f5ab9648a2f4f891a18a08e99184
Dessader <dessader.live@gmail.com>
2022-06-05 12:01:47 +0300

    Implemented preloader component

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,6 +1,17 @@
-import { Navbar } from "@components/js";
+import { Navbar, Preloader } from "@components/js";
+
+const hidePreloaderDelay = 200;
+const preloader = new Preloader(".preloader");
 
 new Navbar(".menu", {
   control: ".menu-control",
   onChange: (menuIsOpen) => menuIsOpen
+});
+
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    preloader.hide(() => {
+      document.body.classList.remove("no-scroll");
+    });
+  }, hidePreloaderDelay);
 });
