9ddece9c68068cf411902ce0af0ffe0ac0ad990d
Dessader <dessader.live@gmail.com>
2022-06-09 01:07:22 +0300

    Added "selected" class to safelist

--- purgecss.config.js
+++ purgecss.config.js
@@ -4,6 +4,6 @@ module.exports = {
   content: ["./build/*.html"],
   css: ["./build/styles/*.css"],
   output: ["./build/styles/"],
-  safelist: [/:active/, "no-scroll"],
+  safelist: [/:active/, "no-scroll", "selected"],
   defaultExtractor: content => content.match(/[A_Za-z0-9-_:/]+/g)
 };
